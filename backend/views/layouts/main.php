7ea3a9adb839c242349b0adc4b6ef5ecc4f6595a
Farindra <farindra.project@gmail.com>
2015-07-31 05:40:48 +0700

    clean

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -21,8 +21,7 @@ LteAsset::register($this);
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    
 </head>
 <body class="skin-blue sidebar-mini">
     <?php $this->beginBody() ?><?= $content ?>
@@ -34,8 +33,6 @@ LteAsset::register($this);
         <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
-    <script src="/7erp/administrator/assets/797d3c26/admin-lte/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
     <script type="text/javascript">
         $( document ).ready(function() {
             $.widget.bridge('uibutton', $.ui.button);
